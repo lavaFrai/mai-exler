e831bb8d8285b003300b734b3b7f4b16ac691a30
lava_frai <lavafrai@yandex.ru>
2024-05-08 02:08:34 +0300

    Autodeploy test

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,4 @@
 docker build .
-docker stop exler-mai
-docker rm exler-mai
-docker run -it -d --restart=always -p 8085:80 --name=exler-mai $(docker build -q .)
+docker stop mai-exler
+docker rm mai-exler
+docker run -it -d --restart=always -p 8085:80 --name=mai-exler $(docker build -q .)
